6d8cc2b29fefe225e423814c3a263e35d58d59ae
David Chen <ychendavid@gmail.com>
2019-03-25 16:51:43 +0800

    Support pm2 cluster mode.

--- fire.sh
+++ fire.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#/bin/bash
 
 # set up the configs for pm2.
 # args: symbols_file_path
+rm ./pm2_configs/*
 python3 config.py --symbols_path $1
 
 # execute processes.
 for entry in "./pm2_configs"/*
 do
 	cp $entry ecosystem.config.js
-	pm2 start ecosystem.config.js
+	pm2 start ecosystem.config.js -i max
 	rm ecosystem.config.js
 done
 
